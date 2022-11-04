41b6cdf625d768f63fb31e782386feef035eee08
LucasMCunha <90118707+LucasMCunha@users.noreply.github.com>
2022-11-04 14:46:53 -0300

    Update Trab.hs

--- Trab.hs
+++ Trab.hs
@@ -16,14 +16,14 @@ Conversor :: ([Int],[Int]) -> [(Int,Int)]
 // de instruções e passa o acumulador junto.
 // Devolve o resultado.
 Main :: [(Int,Int)] -> [(Int,Int)]
-Main (2,x) =  Main Ula Acc LOD x
-Main (4,x) =  Main Ula Acc STO x 
-Main (6,x) =  Main Ula Acc JMP x 
-Main (8,x) =  Main Ula Acc JMZ x 
-Main (10,x) =  Main Ula Acc CPE x 
-Main (14,x) =  Main Ula Acc ADD x 
-Main (16,x) =  Main Ula Acc SUB x 
-Main (18,x) =  Main Ula Acc NOP x 
-Main (20,x) =  Main Ula Acc HLT
+Main (2,x) =  Main Ula ACC y LOD x
+Main (4,x) =  Main Ula ACC y STO x 
+Main (6,x) =  Main Ula ACC y JMP x 
+Main (8,x) =  Main Ula ACC y JMZ x 
+Main (10,x) =  Main Ula ACC y CPE x 
+Main (14,x) =  Main Ula ACC y ADD x 
+Main (16,x) =  Main Ula ACC y SUB x 
+Main (18,x) =  Main Ula ACC y NOP x 
+Main (20,x) =  Main Ula ACC HLT
 
 Ula :: Int -> Command x -> Int -> [(Int,Int)]

746d8c570858507fc279c7e80fdb8598d4135a34
LucasMCunha <90118707+LucasMCunha@users.noreply.github.com>
2022-11-16 01:53:33 -0300

    Update Trab.hs

--- Trab.hs
+++ Trab.hs
@@ -1,10 +1,10 @@
 data Command w = LOD w | STO w | ADD w | SUB w | JMP w | JMZ w | CPE w | NOP w | HTL w deriving Show
 data Registrador y = ACC y | PC y  deriving Show
 
-starter :: ([(Int,Int)],[(Registrador y,Int)]) -> ([(Int,Int)],[(Registrador y,Int)])
-starter (x,[(ACC y,z),(PC a, b)]) | b <= -1 = sum (0,1)
-                                  | otherwise = starter ((replaceNth b h x), regs)
-								    where (h, regs) = cpu ((x !! b), [(ACC y, z), (PC a, b)])
+starter :: ([(Int,Int)],[Registrador y]) -> ([(Int,Int)],[Registrador y])
+starter (x,[(ACC y),(PC z)]) | z <= -1 = sum (0,1)
+                             | otherwise = starter ((replaceNth z h x), regs)
+						 where (h, regs) = cpu ((x !! z), [(ACC y), (PC z)])
 
 replaceNth :: Int -> a -> [a] -> [a]
 replaceNth _ _ [] = []
@@ -12,28 +12,30 @@ replaceNth n newVal (x:xs)
 	| n == 0 = newVal:xs
 	| otherwise = x:replaceNth (n-1) newVal xs
 
-cpu :: ((Int,Int),[(Registrador y,Int)]) -> ((Int,Int),[(Registrador y,Int)])
-cpu ((2,x),[(ACC y,z),(PC a,b)]) = ula ((LOD x,0),[(ACC y,z), (PC a, b)])
-cpu ((4,x),[(ACC y,z),(PC a,b)]) = ula ((STO x,0),[(ACC y,z), (PC a, b)])
-cpu ((6,x),[(ACC y,z),(PC a,b)]) = ula ((JMP x,0),[(ACC y,z), (PC a,b)])
-cpu ((8,x),[(ACC y,z),(PC a,b)]) = ula ((JMZ x,0),[(ACC y,z), (PC a,b)])
-cpu ((10,x),[(ACC y,z),(PC a,b)]) = ula ((CPE x,0),[(ACC y,z), (PC a, b)])
-cpu ((14,x),[(ACC y,z),(PC a,b)]) = ula ((ADD x,0),[(ACC y,z), (PC a, b)])
-cpu ((16,x),[(ACC y,z),(PC a,b)]) = ula ((SUB x,0),[(ACC y,z), (PC a, b)])
-cpu ((18,x),[(ACC y,z),(PC a,b)]) = ula ((NOP x,0),[(ACC y,z), (PC a,b)])
-cpu ((20,x),[(ACC y,z),(PC a,b)]) = ula ((HTL x,0),[(ACC y,z), (PC a,b)])
-ula :: ((Command x,Int),[(Registrador y,Int)]) -> ((Int,Int),[(Registrador y,Int)]) --arrumar saida da ula para coincidir com a saida do cpu
-ula ((LOD x,_),[(ACC y,z), (PC a, b)]) =  [(ACC y,h) | h <- snd (x,_)] --LOD
-ula ((STO x,_),[(ACC y,z),(PC a, b)]) =  [(x,h) | h <- snd (ACC,_) ]--STO
-ula ((JMP x,_),[(ACC y,z),(PC a, b)]) =  (PC y,x) --JMP
-ula ((JMZ x,_),[(ACC y,z),(PC a,b)]) | z == 0 = (PC y,x) --JMZ
-                                   | otherwise = NOP
-ula ((CPE x,_),[(ACC y,z), (PC a, b)]) | z == snd(x,_) = (ACC y, 0) --CPE
-                                     | otherwise = (ACC y,1)
-ula ((ADD x,_),[(ACC y,z), (PC a, b)]) =  [(ACC y,z+h) | h <- snd (x,_)] --ADD
-ula ((SUB x,_),[(ACC y,z), (PC a, b)]) =  [(ACC y,z-h) | h <- snd (x,_)] --SUB
-ula ((NOP x,_),[(ACC y,z), (PC a, b)]) =  (ACC y,0) --NOP
-ula ((HTL x,_),[(ACC y,z), (PC a, b)]) =  (PC y, -1) --HTL
+cpu :: ((Int,Int),[Registrador y]) -> ((Int,Int),[Registrador y])
+cpu ((2,a),[(ACC y),(PC z)]) = ula ((LOD a),[(ACC y),(PC z)])
+cpu ((4,a),[(ACC y),(PC z)]) = ula ((STO a),[(ACC y),(PC z)])
+cpu ((6,a),[(ACC y),(PC z)]) = ula ((JMP a),[(ACC y),(PC z)])
+cpu ((8,a),[(ACC y),(PC z)]) = ula ((JMZ a),[(ACC y),(PC z)])
+cpu ((10,a),[(ACC y),(PC z)]) = ula ((CPE a),[(ACC y),(PC z)])
+cpu ((14,a),[(ACC y),(PC z)]) = ula ((ADD a),[(ACC y),(PC z)])
+cpu ((16,a),[(ACC y),(PC z)]) = ula ((SUB a),[(ACC y),(PC z)])
+cpu ((18,a),[(ACC y),(PC z)]) = ula ((NOP a),[(ACC y),(PC z)])
+cpu ((20,a),[(ACC y),(PC z)]) = ula ((HTL a),[(ACC y),(PC z)])
+
+
+ula :: ((Command w),[Registrador y]) -> ((Int,Int),[Registrador y]) --arrumar saida da ula para coincidir com a saida do cpu
+ula ((LOD w),[(ACC y),(PC z)]) =  [(ACC h) | h <- snd (w,_)] --LOD
+ula ((STO w),[(ACC y),(PC z)]) =  [(w,h) | h <- (ACC _) ]--STO
+ula ((JMP w),[(ACC y),(PC z)])) =  (PC w) --JMP
+ula ((JMZ w),[(ACC y),(PC z)]) | z == 0 = (PC w) --JMZ
+                               | otherwise = NOP
+ula ((CPE w),[(ACC y),(PC z)]) | y == snd(w,_) = (ACC 0) --CPE
+                               | otherwise = (ACC 1)
+ula ((ADD w),[(ACC y),(PC z)]) =  [(ACC y+h) | h <- snd (w,_)] --ADD
+ula ((SUB w),[(ACC y),(PC z)]) =  [(ACC y-h) | h <- snd (w,_)] --SUB
+ula ((NOP w),[(ACC y),(PC z)]) =  (ACC 0) --NOP
+ula ((HTL w),[(ACC y),(PC z)]) =  (PC -1) --HTL
 
 --cpu :: ([(Int,Int)],[(Registrador y,Int)]) -> ([(Int,Int)],[(Registrador y,Int)])
 --cpu ([(2,x)],(ACC y,z)) =  [(ACC y,h) | h <- snd (x,_)] --LOD
